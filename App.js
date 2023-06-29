a62c35270b75378428cb66757583be04a66755ed
Lokpati Tripathi <tripathilokpati@outlook.com>
2023-06-30 00:29:30 +0530

    feat : Header and Body Component

--- App.js
+++ App.js
@@ -1,17 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const HeadingComponent = () => {
-  return <div>Heading Component</div>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const HeadingComponentNew = () => {
-  <React.Fragment>
-    <div id="headerOne">Heading Component</div>;
-    <div id="headerTwo">Heading Component</div>;
-  </React.Fragment>;
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>McDonald's</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponentNew />);
+root.render(<AppLayout />);
