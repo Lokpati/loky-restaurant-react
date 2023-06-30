64b0e1722a630bea0963dcea805f8c44a6f48cc2
Lokpati <lokpati.tripathi@sfaplay.com>
2023-06-30 10:41:12 +0530

    feat : add filter logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,68 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 const Body = () => {
+  let listOfRestaurants = [
+    {
+      type: "restaurant",
+      data: {
+        id: "153030",
+        name: "Vishal Chinese",
+        cloudinaryImageId: "gcrqiuwxfawcou6dvzac",
+        cuisines: ["Chinese"],
+        costForTwo: 50000,
+        deliveryTime: 19,
+        avgRating: 3.8,
+      },
+    },
+    {
+      type: "restaurant",
+      data: {
+        type: "F",
+        id: "153031",
+        name: "McDonald",
+        cloudinaryImageId: "gcrqiuwxfawcou6dvzac",
+        cuisines: ["Chinese,Burger"],
+        costForTwo: 50000,
+        deliveryTime: 19,
+        avgRating: 4,
+      },
+    },
+    {
+      type: "restaurant",
+      data: {
+        type: "F",
+        id: "153033",
+        name: "Burger King",
+        cloudinaryImageId: "gcrqiuwxfawcou6dvzac",
+        cuisines: ["Chinese,Burger"],
+        costForTwo: 50000,
+        deliveryTime: 19,
+        avgRating: 5,
+      },
+    },
+  ];
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {}}>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter logic here
+
+            listOfRestaurants = listOfRestaurants.filter((res) => {
+              return res.data.avgRating >= 5;
+            });
+
+            console.log(listOfRestaurants);
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.idt} resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
-        ;
       </div>
     </div>
   );
