504f07a9f9e0bc10cffd6a5befa15bb788fdfed1
Lokpati Tripathi <tripathilokpati@outlook.com>
2023-06-30 22:33:15 +0530

    feat : login toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
           <li>Contact Us</li>
           <li>About Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              setBtnNameReact(btnNameReact == "Logout" ? "Login" : "Logout");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
