47da85d6b94fca5a194c6ed910fc8611e8cc97a2
Lokpati Tripathi <tripathilokpati@outlook.com>
2023-07-01 22:08:13 +0530

    feat : logo change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LOGO_URL } from "../utils/constants";
+import Logo from "../assets/img/logo-removebg-preview.png";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
@@ -9,13 +9,11 @@ const Header = () => {
   return (
     <div className="flex justify-between bg-purple-400">
       <div className="logo-container">
-        <img className="w-56" src={LOGO_URL}></img>
+        <img className="w-56" src={Logo}></img>
       </div>
       <div className="nav-flex">
         <ul className="flex p-4 m-4 justify-center ">
-          <li className=" p-2 m-2 ">
-            Online Status : {onlineStatus ? "✅" : "🔴"}
-          </li>
+          <li className=" p-2 m-2 ">Status : {onlineStatus ? "✅" : "🔴"}</li>
           <li className=" p-2 m-2 ">
             <Link to="/">Home</Link>
           </li>
@@ -25,7 +23,7 @@ const Header = () => {
           <li className=" p-2 m-2 ">
             <Link to="/about-us">About Us</Link>
           </li>
-          <li className=" p-2 m-2 ">Cart</li>
+          {/* <li className=" p-2 m-2 ">Cart</li> */}
           <button
             className="login"
             onClick={() => {
