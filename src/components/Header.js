c767336515d1b921ac698b40fa08fc3c95a60b38
Lokpati Tripathi <tripathilokpati@outlook.com>
2023-07-01 20:43:04 +0530

    feat : tailwind setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
+    <div className="flex">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL}></img>
       </div>
