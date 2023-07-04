b1c0f926c5721846d54f31992d0a904005c5a0ac
Lokpati <lokpati.tripathi@sfaplay.com>
2023-07-04 10:39:09 +0530

    about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const About = () => {
-  return <div>About Us</div>;
+  const [userDetails, setUserDetails] = useState(null);
+  useEffect(() => {
+    fetchGithubUserDetails();
+  }, []);
+
+  const fetchGithubUserDetails = async () => {
+    const data = await fetch("https://api.github.com/users/loky-sfa");
+    const json = await data.json();
+
+    setUserDetails(json);
+  };
+
+  return userDetails === null ? (
+    <Shimmer />
+  ) : (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto py-8">
+        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <ul className="list-disc pl-6">
+          <li className="text-lg text-gray-700">
+            Loky-sfa, also known as{" "}
+            <span className="font-bold">{userDetails.login}</span> (or Lokpati
+            Tripathi)
+          </li>
+          <li className="text-lg text-gray-700">
+            Talented backend developer with expertise in PHP, Node.js, MySQL,
+            and SQL
+          </li>
+          <li className="text-lg text-gray-700">
+            Proficient in client-side scripting with JavaScript
+          </li>
+          <li className="text-lg text-gray-700">
+            Good knowledge of React and Tailwind CSS
+          </li>
+          <li className="text-lg text-gray-700">
+            Passionate about coding and constantly expanding their skillset
+          </li>
+          <li className="text-lg text-gray-700">
+            Enjoys building efficient and user-friendly web applications
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,4 +1,5 @@
 import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = (props) => {
   const { resData } = props;
 
@@ -10,18 +11,46 @@ const RestaurantCard = (props) => {
     deliveryTime,
     cloudinaryImageId,
   } = resData?.data;
+
+  const renderStarRating = (rating) => {
+    if (rating) {
+      const fullStars = Math.floor(rating);
+      const hasHalfStar = rating % 1 !== 0;
+
+      const starElements = [];
+
+      // Render full stars
+      for (let i = 0; i < fullStars; i++) {
+        starElements.push(<span key={i}>&#9733;</span>);
+      }
+
+      // Render half star
+      if (hasHalfStar) {
+        starElements.push(
+          <span key="halfStar" className="half-star">
+            &#9733;
+          </span>
+        );
+      }
+
+      return starElements;
+    } else {
+      return "Not rated";
+    }
+  };
+
   return (
-    <div className=" w-64 m-4 p-2 rounded-lg bg-gray-100 hover:bg-gray-200 ">
+    <div className="w-64 m-4 p-2 rounded-lg bg-gray-100 hover:bg-gray-200">
       <img
-        className="rounded-lg"
+        className="rounded-lg object-cover w-full h-48"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
-      ></img>
+      />
       <h3 className="font-bold text-lg py-4">{name}</h3>
-      <h4>{cuisines.join(",")}</h4>
-      <h5 className="py-2">{avgRating} stars</h5>
-      <h5>{costForTwo / 100} FOR TWO</h5>
-      <h6 className="py-2">{deliveryTime}minutes</h6>
+      <h4>{cuisines.join(", ")}</h4>
+      <h5 className="py-2">{renderStarRating(avgRating)}</h5>
+      <h5>₹ {costForTwo / 100} FOR TWO</h5>
+      <h6 className="py-2">{deliveryTime} minutes</h6>
     </div>
   );
 };
